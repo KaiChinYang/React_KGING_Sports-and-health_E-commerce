e8316b6a8029017e9c42bd04c974c5ff713a5f95
PaPaYa_KG <T12470503625@gmail.com>
2026-04-04 16:35:24 +0800

    跳轉頁面後回到頁面最上方

--- src/layout/FrontendLayout.jsx
+++ src/layout/FrontendLayout.jsx
@@ -1,11 +1,12 @@
-import React from "react";
 import { Outlet } from "react-router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function FrontendLayout() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <main>
         <Outlet />
